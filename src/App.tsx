34397047fc8c52716bc0a1ae5f1941a3cc1e78f5
Tra Pham <146045106+pctra266@users.noreply.github.com>
2025-07-30 10:39:24 +0700

    feat: complete route, refactor app (#15)

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,13 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import { AuthProvider, useAuth } from './contexts/AuthContext';
-import Home from './pages/Home';
-import Login from './features/Auth/components/Login';
-import NotFound from './pages/NotFound';
-import MainLayout from './layout/MainLayout';
-
-const ProtectedRoute: React.FC<{
-  children: React.ReactNode;
-  requiredRole?: string;
-}> = ({ children, requiredRole }) => {
-  const { isAuthenticated, user } = useAuth();
-
-  if (!isAuthenticated) {
-    return <Navigate to="/login" replace />;
-  }
-
-  if (requiredRole && user?.role !== requiredRole) {
-    return <Navigate to="/" replace />;
-  }
-
-  return <>{children}</>;
-};
+import { AuthProvider} from './contexts/AuthContext';
+import { RouterProvider } from 'react-router-dom';
+import router from "./route/routers"
 
 const App = () => {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router}></RouterProvider>
     </AuthProvider>
   )
 }
-  
+
+export default App
