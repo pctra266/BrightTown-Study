0ed1f71d13a0d91340ec37bf1669acb671996aa1
Tra Pham <phamtra001@gmail.com>
2025-07-31 13:20:38 +0700

    feat: add Library UI

--- src/route/routers.tsx
+++ src/route/routers.tsx
@@ -3,6 +3,11 @@ import MainLayout from "../layout/MainLayout";
 import Home from "../pages/Home";
 import Login from "../features/Auth/components/Login";
 import NotFound from "../pages/NotFound";
+import Admin from "../features/AdminDashboard/Admin";
+import ManagerUser from "../features/AdminDashboard/ManagerUser";
+import ManageBooks from "../pages/ManageBooks";
+import Library from "../pages/Library";
+
 
 const routers = createBrowserRouter([
   {
@@ -12,6 +17,10 @@ const routers = createBrowserRouter([
       { path: "/", element: <Home /> },
       { path: "home", element: <Home /> },
       { path: "login", element: <Login /> },
+      { path: "admin", element: <Admin /> },
+      { path: "library", element: <Library /> },
+      { path: "manageuser", element: <ManagerUser /> },
+      { path: "book", element: <ManageBooks /> },
       { path: "*", element: <NotFound /> },
     ],
   },
