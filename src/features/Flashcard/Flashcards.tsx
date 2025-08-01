6d9e5aff55370525c7c4c3938e9186671613b4b3
Tra Pham <phamtra001@gmail.com>
2025-08-01 14:12:04 +0700

    fix: fix path to unknown services

--- src/features/Flashcard/Flashcards.tsx
+++ src/features/Flashcard/Flashcards.tsx
@@ -1,17 +1,9 @@
 import FlashcardSets from "./components/FlashcardSets";
-import { deleteFlashcardSet } from "./services/flashcardService";
-
-import { useNavigate } from "react-router-dom";
 import { useFlashcardSets } from "./hooks/useFlashcardSets";
-import { useAuth } from "../../contexts/AuthContext";
 
 const Flashcards = () => {
-  const navigate = useNavigate();
-  const { user } = useAuth();
-  const userId = user?.id || "";
-  const userRoleId = user?.role || "";
 
-  const {flashcardSets, fetchData:fetchFlashcardSetData} = useFlashcardSets();
+  const {flashcardSets} = useFlashcardSets();
 
   const handleCreate = () => {console.log("go to create Page")};
   const handleDelete = async (id: string) => {console.log("delete",id)};
