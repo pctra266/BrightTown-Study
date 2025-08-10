a8d013b83900905fb88b7d5a6a474b50225a6a46
Tra Pham <phamtra001@gmail.com>
2025-08-10 23:17:22 +0700

    feat: chat realtime

--- src/features/Auth/services/firebase.ts
+++ src/features/Auth/services/firebase.ts
@@ -1,9 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
-import { getFirestore } from "firebase/firestore";
+import { getDatabase  } from "firebase/database";
 const firebaseConfig = {
   apiKey: "AIzaSyB2H2JfWr58bpsYpF2RlM16SugsZF3VnM0",
   authDomain: "authentication-314b9.firebaseapp.com",
+  databaseURL: "https://authentication-314b9-default-rtdb.asia-southeast1.firebasedatabase.app/",
   projectId: "authentication-314b9",
   storageBucket: "authentication-314b9.firebasestorage.app",
   messagingSenderId: "1000675315972",
@@ -12,6 +13,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
- const db = getFirestore(app);
+const db = getDatabase(app);
 
 export { auth,db };
